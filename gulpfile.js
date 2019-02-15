571a20b6a736e8a0b85cefbcaea3fcd0ff981829
taleb <taleb@lenovo-linux-mint-19.1>
2019-02-15 20:54:35 +0000

    Customise header image

--- gulpfile.js
+++ gulpfile.js
@@ -66,7 +66,7 @@ gulp.task('css:compile', function() {
 });
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
+gulp.task('css:minify', gulp.series('css:compile', function(done) {
   return gulp.src([
       './css/*.css',
       '!./css/*.min.css'
@@ -77,13 +77,13 @@ gulp.task('css:minify', ['css:compile'], function() {
     }))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
-});
+}));
 
 // CSS
-gulp.task('css', ['css:compile', 'css:minify']);
+gulp.task('css', gulp.series('css:compile', 'css:minify'));
 
 // Minify JavaScript
-gulp.task('js:minify', function() {
+gulp.task('js:minify', function(done) {
   return gulp.src([
       './js/*.js',
       '!./js/*.min.js'
@@ -100,23 +100,25 @@ gulp.task('js:minify', function() {
 });
 
 // JS
-gulp.task('js', ['js:minify']);
+gulp.task('js', gulp.series('js:minify'));
 
 // Default task
-gulp.task('default', ['css', 'js', 'vendor']);
+gulp.task('default', gulp.series('css', 'js', 'vendor'));
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function(done) {
   browserSync.init({
     server: {
       baseDir: "./"
     }
   });
+  done();
 });
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function() {
+gulp.task('dev', gulp.series('css', 'js', 'browserSync', function(done) {
   gulp.watch('./scss/*.scss', ['css']);
   gulp.watch('./js/*.js', ['js']);
   gulp.watch('./*.html', browserSync.reload);
-});
+  done();
+}));
